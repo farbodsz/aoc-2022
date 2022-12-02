bcf380d764fc22f95b505c6bb50daab4a3ff1b9b
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2022-12-02 01:29:28 +0000

    Complete day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,6 +1,31 @@
-module Lib
-    ( someFunc
-    ) where
+--------------------------------------------------------------------------------
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Day01 (solveA, solveB) where
+
+import Data.List (sortOn)
+import Data.Ord (Down (Down))
+import Data.Text (Text)
+import Data.Text qualified as T
+import Util
+
+--------------------------------------------------------------------------------
+
+solveA :: Text -> Text
+solveA = tshow . maximum . groupNums
+
+groupNums :: Text -> [Int]
+groupNums = map (sum . map tread) . splitLines . T.lines
+
+splitLines :: [Text] -> [[Text]]
+splitLines ls =
+    let (xs, ys) = span (/= "") ls
+     in case ys of
+            [] -> [xs]
+            _ : ys' -> xs : splitLines ys'
+
+--------------------------------------------------------------------------------
+
+solveB :: Text -> Text
+solveB = tshow . sum . take 3 . sortOn Down . groupNums
+
+--------------------------------------------------------------------------------
